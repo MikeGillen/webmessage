6b6cd18481426116c0cdc760ccd16fb97cfd7a07
MikeXCZ <mike.gillenduchalais.pro@gmail.com>
2025-03-01 13:57:17 +0100

    .sh server

--- server.sh
+++ server.sh
@@ -4,6 +4,5 @@ fuser -k 3000/tcp 2>/dev/null # Tue tout processus utilisant le port 3000
 echo "Starting the Node.js server..."
 cd /home/WorkaMikeLukas/Desktop/webmessage || exit
 
-# Démarrer le serveur et enregistrer les logs
-nohup node ./server.js > server.log 2>&1 &
-echo "Server started successfully!"
+# Run Node.js server and log output
+node ./server.js >> server.log 2>&1 &
